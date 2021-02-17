8351d43f0d7861733eff8715c54b44264b06f07a
Marielle Akser <marielle.akser@gmail.com>
2021-02-17 16:22:08 +0100

    created files

---
+++ detector_test/detector_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  detector_test
+//
+//  Created by Marielle Aksér on 2021-02-17.
+//  Copyright © 2021 Marielle Aksér. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
