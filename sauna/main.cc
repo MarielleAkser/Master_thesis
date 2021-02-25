7053bc0e07d6bb3d9b0a35315096d4c91419d797
Marielle Akser <marielle.akser@gmail.com>
2021-02-25 10:53:17 +0100

    Changed names

--- sauna/main.cc
+++ sauna/main.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-/// \file examplesauna.cc
+/// \file main.cc
 /// \brief Main program of the sauna example
 
 #include "saunaDetectorConstruction.hh"
